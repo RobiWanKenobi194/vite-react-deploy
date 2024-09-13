63888a2b1f030819555eb1587bfe4b6cee1bce90
RobiWanKenobi194 <robinrocker@icloud.com>
2024-09-13 13:31:45 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,18 +16,19 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Krebsi Counter 🦀</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          click to increase the Krebsi counter
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Klick die Logos an um mehr zu erfahren.
       </p>
+      <p>GaLiGrü Krebsi </p>
     </>
   )
 }
